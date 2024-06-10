a82549202c8b13172e13a4c56184c72aaec9bf02
Aladser <Aladser@users.noreply.github.com>
2024-06-10 14:29:51 +0900

    добавлен post-запрос

--- main/views.py
+++ main/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        text = request.POST['text']
+        print(f"{email} {text}")
+
     return render(request, 'main/index.html')
