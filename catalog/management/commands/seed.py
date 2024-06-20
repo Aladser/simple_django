b4c4c428be5abe3b9ff1efba60edc2ab5ab792ca
Aladser <aladser@gmail.com>
2024-06-21 07:07:02 +0900

    обновлен seed

--- catalog/management/commands/seed.py
+++ catalog/management/commands/seed.py
@@ -10,9 +10,9 @@ class Command(BaseCommand):
 
         # Категории
         category_list = [
-            {'name': 'бытовая химия'},
-            {'name': 'автомобили'},
-            {'name': 'продукты'},
+            {'pk': 1, 'name': 'автомобили'},
+            {'pk': 2, 'name': 'бытовая химия'},
+            {'pk': 3, 'name': 'продукты'},
         ]
         category_create_list = [Category(**ctg) for ctg in category_list]
         Category.objects.bulk_create(category_create_list)
@@ -22,9 +22,9 @@ class Command(BaseCommand):
             category_list[ctg.name] = ctg.pk
 
         product_list = [
-            {'name': 'хлеб', 'category_id': category_list['продукты']},
-            {'name': 'шампунь', 'category_id': category_list['бытовая химия']},
-            {'name': 'Жигули', 'category_id': category_list['автомобили']},
+            {'pk': 1, 'name': 'Жигули', 'category_id': category_list['автомобили']},
+            {'pk': 2, 'name': 'Хлеб', 'category_id': category_list['продукты']},
+            {'pk': 3, 'name': 'Шампунь', 'category_id': category_list['бытовая химия']},
         ]
         product_create_list = [Product(**prd) for prd in product_list]
         Product.objects.bulk_create(product_create_list)
