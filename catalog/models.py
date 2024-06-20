9c582b16d5fcdaec8fb58e7a1911128ab519dd09
Aladser <aladser@gmail.com>
2024-06-21 04:04:10 +0900

    добавлены модели Product, Category

--- catalog/models.py
+++ catalog/models.py
@@ -1,17 +1,42 @@
 from django.db import models
 
+NULLABLE = {
+    'null': True,
+    'blank': True
+}
 
-# Create your models here.
-class Student(models.Model):
+
+class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name='Имя')
-    surname = models.CharField(max_length=100, verbose_name='Фамилия')
-    ava = models.ImageField(upload_to='students/', verbose_name='Аватар', null=True, blank=True)
+    description = models.CharField(max_length=256, verbose_name='Описание',**NULLABLE)
     is_active = models.BooleanField(default=True, verbose_name='Активен')
 
     def __str__(self):
-        return f"{self.name} {self.surname}"
+        return self.name
 
     class Meta:
-        verbose_name = 'Студент'
-        verbose_name_plural = 'Студенты'
-        ordering = ('surname',)
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Имя')
+    description = models.CharField(max_length=256, verbose_name='Описание', **NULLABLE)
+    ava = models.ImageField(upload_to='img/products', verbose_name='Аватар', **NULLABLE)
+    price = models.IntegerField(verbose_name='Цена за покупку', **NULLABLE)
+    is_active = models.BooleanField(default=True, verbose_name='Активен')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(
+        to=Category,
+        on_delete=models.CASCADE,
+        related_name='products',
+        verbose_name='категория',
+    )
+
+    def __str__(self):
+        return f"{self.name} [{self.price} {self.category}]"
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
