3c950302ffcfb0693d8b21fd9bc69943a359e7bf
Aladser <aladser@gmail.com>
2024-06-21 08:25:02 +0900

    обновлены сиды

--- catalog/management/commands/seed.py
+++ catalog/management/commands/seed.py
@@ -4,29 +4,27 @@ from catalog.models import Category, Product
 
 
 class Command(BaseCommand):
+    # Категории
+    category_list = [
+        {'pk': 1, 'name': 'автомобили'},
+        {'pk': 2, 'name': 'бытовая химия'},
+        {'pk': 3, 'name': 'продукты'},
+    ]
+    # Товары
+    product_list = [
+        {'pk': 1, 'name': 'Жигули', 'category_id': 1},
+        {'pk': 2, 'name': 'Хлеб', 'category_id': 3},
+        {'pk': 3, 'name': 'Шампунь', 'category_id': 2},
+    ]
+
     def handle(self, *args, **kwargs):
         Product.objects.all().delete()
         Category.objects.all().delete()
 
-        # Категории
-        category_list = [
-            {'pk': 1, 'name': 'автомобили'},
-            {'pk': 2, 'name': 'бытовая химия'},
-            {'pk': 3, 'name': 'продукты'},
-        ]
-        category_create_list = [Category(**ctg) for ctg in category_list]
+        category_create_list = [Category(**ctg) for ctg in self.category_list]
         Category.objects.bulk_create(category_create_list)
 
-        category_list = {}
-        for ctg in Category.objects.all():
-            category_list[ctg.name] = ctg.pk
-
-        product_list = [
-            {'pk': 1, 'name': 'Жигули', 'category_id': category_list['автомобили']},
-            {'pk': 2, 'name': 'Хлеб', 'category_id': category_list['продукты']},
-            {'pk': 3, 'name': 'Шампунь', 'category_id': category_list['бытовая химия']},
-        ]
-        product_create_list = [Product(**prd) for prd in product_list]
+        product_create_list = [Product(**prd) for prd in self.product_list]
         Product.objects.bulk_create(product_create_list)
 
 
--- catalog/management/commands/seed_json.py
+++ catalog/management/commands/seed_json.py
@@ -8,22 +8,27 @@ from catalog.models import Category, Product
 class Command(BaseCommand):
 
     @staticmethod
-    def json_read_data(filename='data.json'):
+    def json_read_data():
         """получает данные из JSON файла"""
         data = {'categories': [], 'products': []}
 
-        with open(filename, 'r', encoding='utf8') as file:
+        with open('data.json', 'r', encoding='utf8') as file:
             file_data = json.load(file)
 
         for elem in file_data:
             if elem['model'] == 'catalog.category':
-                data['categories'].append(elem['fields']['name'])
+                data['categories'].append({'pk': elem['pk'], 'name': elem['fields']['name']})
             else:
-                data['products'].append({'name': elem['fields']['name'], 'category': elem['fields']['category']})
+                data['products'].append({'name': elem['fields']['name'], 'category_id': elem['fields']['category']})
         return data
 
     def handle(self, *args, **kwargs):
+        Product.objects.all().delete()
+        Category.objects.all().delete()
+
         json_data = self.json_read_data()
 
-        [print(el) for el in json_data['categories']]
-        [print(el) for el in json_data['products']]
+        ctg_obj_list = [Category(**ctg) for ctg in json_data['categories']]
+        Category.objects.bulk_create(ctg_obj_list)
+        prd_obj_list = [Product(**prd) for prd in json_data['products']]
+        Product.objects.bulk_create(prd_obj_list)
