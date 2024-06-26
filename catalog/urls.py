185cf9206418e9cb7d4ff592613594b26159d4a8
Aladser <aladser@gmail.com>
2024-06-26 15:35:24 +0900

    добавлена страница товара

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from catalog.views import index, contacts
+from catalog.views import index, contacts, product_show
 
 urlpatterns = [
     path('', index),
+    path('products/<int:pk>', product_show),
     path('contacts/', contacts),
     path('admin/', admin.site.urls)
 ]
